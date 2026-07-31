cb2c5b1d535d8f0fe4a2a07104af68d54976138a
Sergey Frolov <sergfrol33@gmail.com>
2026-07-31 22:07:16 +0300

    выполнил задание 2.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        TestTask testTask = new TestTask();
+        testTask.printThreeWords();
+        testTask.checkSumSign();
+        testTask.printColor();
+        testTask.compareNumbers();
+        System.out.println(testTask.compareNumbersRange(10,19));
+        testTask.isPositiveNumberPrint(3);
+        System.out.println(testTask.isPositiveNumberBool(3));
+        testTask.printTextUntil(4,"anime otstoi");
+        System.out.println(testTask.isLeapYear(2012));
+        testTask.updateArray();
+        testTask.mapArray();
+        testTask.createMatrix();
+        System.out.println(Arrays.toString(testTask.createFillArray(4,1)));
     }
+
 }
